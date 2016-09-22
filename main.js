1b2f3161f9c221de8c56422621a81413d8f8ff71
Sebastian <sebastian_cheung@live.com>
2016-09-22 18:11:19 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,70 @@
+/**
+ * Created by sebastien.cheung on 22/09/2016.
+ */
+// https://www.amcharts.com/kbase/your-first-chart-with-amcharts/
+
+
+import AmCharts from 'amcharts3';
+
+var chartData = [ {
+    "country": "USA",
+    "visits": 4252
+}, {
+    "country": "China",
+    "visits": 1882
+}, {
+    "country": "Japan",
+    "visits": 1809
+}, {
+    "country": "Germany",
+    "visits": 1322
+}, {
+    "country": "UK",
+    "visits": 1122
+}, {
+    "country": "France",
+    "visits": 1114
+}, {
+    "country": "India",
+    "visits": 984
+}, {
+    "country": "Spain",
+    "visits": 711
+}, {
+    "country": "Netherlands",
+    "visits": 665
+}, {
+    "country": "Russia",
+    "visits": 580
+}, {
+    "country": "South Korea",
+    "visits": 443
+}, {
+    "country": "Canada",
+    "visits": 441
+}, {
+    "country": "Brazil",
+    "visits": 395
+}, {
+    "country": "Italy",
+    "visits": 386
+}, {
+    "country": "Australia",
+    "visits": 384
+}, {
+    "country": "Taiwan",
+    "visits": 338
+}, {
+    "country": "Poland",
+    "visits": 328
+} ];
+
+AmCharts.makeChart("chartdiv", {
+    "type": "serial",
+    "dataProvider": chartData,
+    "categoryField": "country",
+    "graph": [{
+        "valueField": "visits",
+        "type": "column"
+    }]
+});
